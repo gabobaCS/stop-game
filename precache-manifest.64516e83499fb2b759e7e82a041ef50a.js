fde4207defa356ebe902fff0826d9754e9388f6e
Idan Hershcovich <idanhershcovich@idans-mbp-16.lan>
2020-03-20 04:03:21 -0400

    Updates

---
+++ precache-manifest.64516e83499fb2b759e7e82a041ef50a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1b81dabc8fb54bdb87fad14087b10c",
+    "url": "/stop_game/index.html"
+  },
+  {
+    "revision": "c1b40e71f65d279d928c",
+    "url": "/stop_game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edf29b5048d2ff0cda59",
+    "url": "/stop_game/static/js/2.5dac1c6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stop_game/static/js/2.5dac1c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b40e71f65d279d928c",
+    "url": "/stop_game/static/js/main.fda34da4.chunk.js"
+  },
+  {
+    "revision": "f0d19431dec39a91d0ec",
+    "url": "/stop_game/static/js/runtime-main.9815174d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stop_game/static/media/logo.5d5d9eef.svg"
+  }
+]);
